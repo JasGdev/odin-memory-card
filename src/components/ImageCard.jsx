8dc954879d8513e60e958811d66cd8fbff9a13b6
Jason <jason13.deng@gmail.com>
2026-02-24 15:16:48 +0900

    feat: finish final implementation/fix img sizing

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -6,21 +6,21 @@ import "./styles/ImageCard.css";
 function ImageCard({ prompt, onClick}) {
 	const [imgSrc, setImgSrc] = useState(null);
 
-	// useEffect(() => {
-	// 	async function getImg() {
-	// 		try {
-	// 			const response = await fetch(
-	// 				`https://api.giphy.com/v1/gifs/translate?api_key=oJnG1fKVm1cBr27Usk1i24OT1zWwQveE&s=${prompt}`,
-	// 			);
-	// 			const imgData = await response.json();
-	// 			const imgUrl = imgData.data.images.original.url
-	// 			setImgSrc(imgUrl);
-	// 		} catch (error) {
-	// 			console.log("Error");
-	// 		}
-	// 	}
-	// 	getImg();
-	// }, [prompt]);
+	useEffect(() => {
+		async function getImg() {
+			try {
+				const response = await fetch(
+					`https://api.giphy.com/v1/gifs/translate?api_key=oJnG1fKVm1cBr27Usk1i24OT1zWwQveE&s=${prompt}`,
+				);
+				const imgData = await response.json();
+				const imgUrl = imgData.data.images.original.url
+				setImgSrc(imgUrl);
+			} catch (error) {
+				console.log("Error");
+			}
+		}
+		getImg();
+	}, [prompt]);
 
 	return (
 		<>
@@ -28,7 +28,7 @@ function ImageCard({ prompt, onClick}) {
 				onClick={onClick}
 				className="imageCardContainer"
 			>
-				<img src={blackImg} alt="CAT" />
+				<img src={imgSrc} alt="CAT" />
 				<div className="imgLabel">{prompt}</div>
 			</div>
 		</>
